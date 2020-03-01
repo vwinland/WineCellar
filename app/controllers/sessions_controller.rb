37845896dd74b85b730e367d44495ec7bf7f07d9
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-01 18:26:38 -0500

    adds authentication in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+
+    def new 
+    end
+
+    def create 
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            sessions[:user_id] = @user_id
+            redirect_to '/'
+        else
+            redirect_to '/login'
+        end
+    end
 end
