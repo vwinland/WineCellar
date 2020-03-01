0f968fb39e2f86eddbc1ac85541f9794dbf3e29e
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-01 18:23:34 -0500

    adds secure password to users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do  
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'welcome#home'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destory'
+  get '/signup' => "users#new"
+  post '/signup' => 'users#create'
 end
