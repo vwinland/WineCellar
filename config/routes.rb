4b08bd421033630ad4b2b758f006167abe0a054a
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-01 19:03:27 -0500

    installs bcrypt gem and adds new user form to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do  
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'welcome#home'
+  get '/register', to: 'users#new'
+  #root 'welcome#home'
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destory'
-  get '/signup' => "users#new"
-  post '/signup' => 'users#create'
+  #get '/login' => 'sessions#new'
+  #post '/login' => 'sessions#create'
+  #post '/logout' => 'sessions#destory'
+  #get '/signup' => "users#new"
+  #post '/signup' => 'users#create'
 end
