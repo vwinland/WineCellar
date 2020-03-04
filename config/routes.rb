d062838d56ef79b35e9ca5879bdc95977bd33512
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-04 17:18:19 -0500

    cleans up new view for wines

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do  
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :reviews do 
-    resources :wines, only: [:new]
+  resources :wines do 
+    resources :reviews
   end
   get '/register', to: 'users#new'
   #root 'welcome#home'
