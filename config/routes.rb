b6af3673bb9cc138d902636f026c6437c7c03ef0
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-05 20:45:49 -0500

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do  
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :wines do 
-    resources :reviews
-  end
+
   get '/register', to: 'users#new'
   #root 'welcome#home'
   post '/register', to: 'users#create'
 
-  get '/wines', to: "wines#index"
-  #get '/wines/new', to: 'wines#new'
+  resources :wines, only: :index
 
-  #get '/reviews', to: "reviews#new"
-  #post '/reviews', to: 'reviews#create'
-  #get '/wine', to: 'wines#index'
-  #get '/login' => 'sessions#new'
-  #post '/login' => 'sessions#create'
-  #post '/logout' => 'sessions#destory'
-  #get '/signup' => "users#new"
-  #post '/signup' => 'users#create'
 end
