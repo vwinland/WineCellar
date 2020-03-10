c359a301524f0fa6e7f24ba5aba5e19a4544d9ad
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-09 20:45:22 -0400

    adds scope methods to wine controller

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,19 @@
 class Wine < ApplicationRecord
     has_many :reviews
     #has_many :users, through: :reviews
+
+    def self.red
+        where(grape_variety: 'Red')
+    end
+
+    def self.white
+        where(grape_variety: 'White')
+    end
+
+    def self.sparkling
+        where(grape_variety: 'Sparkling')
+    end
+
+
+    #scope :wine => { where(grape_variety: 'Red') }
 end
