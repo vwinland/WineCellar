3dea2b8ade921f870f188376e661a8a8c41083be
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-10 18:28:21 -0400

    adds abstract scope methods to wine model

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,18 +3,8 @@ class Wine < ApplicationRecord
     #has_many :users, through: :reviews
     validates_presence_of :name
 
-    def self.red
-        where(grape_variety: 'Red')
-    end
-
-    def self.white
-        where(grape_variety: 'White')
-    end
-
-    def self.sparkling
-        where(grape_variety: 'Sparkling')
-    end
-
-
-    #scope :wine => { where(grape_variety: 'Red') }
+    scope :red, -> { where(grape_variety: 'Red') }
+    scope :white, -> { where(grape_variety: 'White') }
+    scope :sparkling, -> { where(grape_variety: 'Sparkling')} 
+  
 end
