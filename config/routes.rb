86ce8ab8cf4bb0ec166e5be79ac0077f35962946
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-10 22:13:05 -0400

    updates user routes in config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do  
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get '/register', to: 'users#new'
-  #root 'welcome#home'
-  post '/register', to: 'users#create'
-
   resources :wines, only: [:index, :new, :create] do 
     resources :reviews, only: [:index, :new]
   end
@@ -13,5 +7,11 @@ Rails.application.routes.draw do
   get '/wines/red', to: 'wines#red' 
   get '/wines/white', to: 'wines#white'
   get '/wines/sparkling', to: 'wines#sparkling'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
 end
