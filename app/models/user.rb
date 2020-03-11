c6ca4e39fd7436486ae421d4158acfa23d83dba3
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-03-10 22:14:02 -0400

    adds validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reviews
     has_many :wines, through: :reviews
+    validates_presence_of :name, :password, :password_confirmation
+
 end
